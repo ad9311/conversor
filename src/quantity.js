fced011267085ef1d30ef804c6010d37c306ae16
Ángel Díaz <diazangel9311@gmail.com>
2021-08-29 18:34:26 -0500

    Add all area unit table entries
    Rearrange quanity.js to be more readable

--- src/quantity.js
+++ src/quantity.js
@@ -1,13 +1,52 @@
 class Quantity {
   getUnits = () => {
     const units = {
-      Area: ['Acre', 'Hectare', 'Square Centimeter', 'Square Foot',
-        'Square Inch', 'Square Kilometer', 'Square Meter', 'Square Mile', 'Square Yard'],
-      Mass: ['Carat', 'Centigram', 'Gram', 'Kilogram', 'Miligram', 'Pound', 'Ton(Metric)'],
-      Length: ['Centimeter', 'Foot', 'Inch', 'Kilometer', 'Meter', 'Mile', 'Yard'],
-      Temperature: ['Celsius', 'Fahrenheit', 'Kelvin', 'Rankine'],
-      Volume: ['Cubic Centimeter', 'Cubic Foot', 'Cubic Inch', 'Cubic Meter',
-        'Gallon', 'Liter', 'Milliliter', 'Ounce', 'Tablespoon'],
+      Area: [
+        'Acre',
+        'Hectare',
+        'Square Centimeter',
+        'Square Foot',
+        'Square Inch',
+        'Square Kilometer',
+        'Square Meter',
+        'Square Mile',
+        'Square Yard',
+      ],
+      Mass: [
+        'Carat',
+        'Centigram',
+        'Gram',
+        'Kilogram',
+        'Miligram',
+        'Pound',
+        'Ton(Metric)',
+      ],
+      Length: [
+        'Centimeter',
+        'Foot',
+        'Inch',
+        'Kilometer',
+        'Meter',
+        'Mile',
+        'Yard',
+      ],
+      Temperature: [
+        'Celsius',
+        'Fahrenheit',
+        'Kelvin',
+        'Rankine',
+      ],
+      Volume: [
+        'Cubic Centimeter',
+        'Cubic Foot',
+        'Cubic Inch',
+        'Cubic Meter',
+        'Gallon',
+        'Liter',
+        'Milliliter',
+        'Ounce',
+        'Tablespoon',
+      ],
     };
     return units;
   };
