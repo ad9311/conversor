cb88e711626cdef29252b5665e1d302f135318ca
Ángel Díaz <diazangel9311@gmail.com>
2021-08-28 19:55:43 -0500

    Fix linter issues

--- src/renderer.js
+++ src/renderer.js
@@ -1,20 +1,21 @@
 class Renderer {
-  clearSelect() {
+  clearSelect = () => {
     document.getElementById('source').innerHTML = '';
     document.getElementById('target').innerHTML = '';
   }
 
-  appendOption(id, unit) {
+  appendOption = (id, unit) => {
     const selectTag = document.getElementById(id);
     const option = document.createElement('option');
     option.value = unit;
     option.innerHTML = unit;
     selectTag.appendChild(option);
   }
+
   renderSelect(units) {
     this.clearSelect();
     const selection = document.getElementById('quantities').value;
-    units[selection].forEach(unit => {
+    units[selection].forEach((unit) => {
       this.appendOption('source', unit);
       this.appendOption('target', unit);
     });
